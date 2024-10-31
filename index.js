0aafaeba36ce34cfdd4808860f2b8c528257a103
Ari <arina.tupotilova@gmail.com>
2024-10-31 17:05:21 +0300

    next

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hi!')
+})
+
+app.get('/registration', (req, res) => {
+  res.sendFile("index.html")
+})
+
+app.get('/ola', (req, res) => {
+  res.send('ola!')
+})
+
+app.get('/hei', (req, res) => {
+  res.status(200).send('hei! GET request received')
+})
+
+app.post('/hei', (req, res) => {
+  res.status(202).send('hei! POST request received')
+})
+
+app.put('/hei', (req, res) => {
+  res.status(204).send('hei! PUT request received')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
