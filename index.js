38e16065cae0b38fb6ff045677735484a1fa1a15
Ari <arina.tupotilova@gmail.com>
2024-11-01 14:11:30 +0300

    updated

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const express = require('express')
 const path = require("path")
+const fs = require("fs")
 const app = express()
 const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
   res.send('hi!')
 })
 
-app.get('/registration', (req, res) => {
-  res.sendFile("project.html", {root: path.join(__dirname)})
+app.get('*', (req, res) => {
+  res.sendFile("project.html", {root: path.join(__dirname, "./static")})
 })
 
-app.get('/ola', (req, res) => {
-  res.send('ola!')
+app.post('/reg-data', (req, res) => {
+  console.log(">>>>>>>>>>>>>", req.body)
+  fs.appendFile("data.txt", JSON.stringify(req.body), (err)=>{
+    if(err) {
+      res.status(500).send("user not added")
+    } else {
+      res.status(201).send("user added")
+    }
+  })
 })
-
-app.get('/hei', (req, res) => {
-  res.status(200).send('hei! GET request received')
-})
-
-app.post('/hei', (req, res) => {
-  res.status(202).send('hei! POST request received')
-})
-
-app.put('/hei', (req, res) => {
-  res.status(204).send('hei! PUT request received')
-})
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
